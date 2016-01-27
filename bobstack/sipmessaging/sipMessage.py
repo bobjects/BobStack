826dee3fc5e8b014dfd2bc6125a771c4951f0e10
bobjects <bob@bobjectsinc.com>
2016-01-26 21:37:33 -0600

    sipmessaging work in progress.

--- bobstack/sipmessaging/sipMessage.py
+++ bobstack/sipmessaging/sipMessage.py
@@ -1,5 +1,34 @@
+from cStringIO import StringIO
 from sipStartLine import SIPStartLine
 
 class SIPMessage(object):
-    # TODO
-    pass
+    def __init__(self, aString, aSIPStartLine):
+        self.rawString = aString
+        self.startLine = aSIPStartLine
+        # TODO:  SIP messages are terminted by CRLF.  does readline strip both the CR and LF?  That's what we want.
+        self.content = ""
+        self.headerFieldLines = []
+        with StringIO(aString) as stringIO:
+            stringIO.readline()  # skip the first line.  We've already parsed it.
+            lineString = stringIO.readline()
+            while lineString.__len__() > 0:
+                self.headerFieldLines.append(lineString)
+                lineString = stringIO.readline()
+            self.content = stringIO.read()
+        self.headerFields = [self.headerFieldForLine(line) for line in self.headerFieldLines]
+
+    @property
+    def isValid(self):
+        return False
+
+    @property
+    def isKnown(self):
+        return False
+
+    @property
+    def isMalformed(self):
+        return False
+
+    def headerFieldForLine(self, aString):
+        # TODO
+        pass
--- bobstack/sipmessaging/sipMessageFactory.py
+++ bobstack/sipmessaging/sipMessageFactory.py
@@ -1,21 +1,27 @@
 # from stringBuffer import StringBuffer
 # from protoSIPMessage import ProtoSIPMessage
+from cStringIO import StringIO
 from malformedSIPStartLine import MalformedSIPStartLine
 from sipRequestStartLine import SIPRequestStartLine
 from sipResponseStartLine import SIPResponseStartLine
 from unknownSIPRequest import UnknownSIPRequest
 from unknownSIPResponse import UnknownSIPResponse
 from malformedSIPMessage import MalformedSIPMessage
+from eventSourceMixin import EventSourceMixin
 
-
-class SIPMessageFactory(object):
+class SIPMessageFactory(EventSourceMixin):
     def __init__(self):
         # TODO
         pass
 
     def nextPutAll(self, aString):
-        # TODO
-        pass
+        # TODO:  SIP messages are terminted by CRLF.  does readline strip both the CR and LF?  That's what we want.
+        with StringIO(aString) as stringIO:
+            firstLine = stringIO.readline()
+        sipStartLine = self.sipStartLineClassForString(firstLine)(firstLine)
+        sipMessage = self.sipMessageClassForStartLine(sipStartLine)(aString, sipStartLine)
+        self.triggerEventForSIPMessage(sipMessage)
+        return sipMessage
 
     def sipStartLineClassForString(self, aString):
         if SIPRequestStartLine.matchesLine(aString):
@@ -39,3 +45,15 @@ class SIPMessageFactory(object):
                 return UnknownSIPResponse
         else:
             return MalformedSIPMessage
+
+    def triggerEventForSIPMessage(self, aSIPMessage):
+        if aSIPMessage.isMalformed:
+            self.triggerEvent("malformedSIPMessage")
+        if aSIPMessage.isValid:
+            self.triggerEvent("validSIPMessage")
+            if aSIPMessage.isKnown:
+                self.triggerEvent("validKnownSIPMessage")
+            else:
+                self.triggerEvent("validUnknownSIPMessage")
+        else:
+            self.triggerEvent("invalidSIPMessage")
