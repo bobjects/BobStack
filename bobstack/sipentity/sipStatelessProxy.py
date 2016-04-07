934302bd14a8b5e2be70d6dcb0bd7aea13ab6459
bobjects <bob@bobjectsinc.com>
2016-04-07 00:11:57 -0500

    working on sip stateless proxy.

--- bobstack/sipentity/sipStatelessProxy.py
+++ bobstack/sipentity/sipStatelessProxy.py
@@ -9,6 +9,15 @@ class SIPStatelessProxy(SIPEntity):
         # TODO - do a lot of cool stuff with the request.  Add headers, forward it downstream, etc.
         # - If the transport connection isStateful, get the ID of the transport, and use it ensure that
         #   our response goes back through the correct transport connection.
+        # - Validate the request - https://tools.ietf.org/html/rfc3261#section-16.3
+        #     - isMalformed is False
+        #     - Check for a merged request (i.e. 482 (Loop Detected))
+        #     - Check the request URI scheme, to ensure that we understand it (i.e. "sip" or "sips") 416 (Unsupported URI Scheme)
+        #     - Check Max-Forwards.  If 0, don't forward the request, 483 (Too Many Hops)
+        #     - (optional) Loop check.  Via header with sent-by value that's already been placed into previous requests by us
+        #        - Maybe only appropriate for stateful proxy.
+        #     - Proxy-Require test - 420 (Bad Extension)
+        #     - Proxy-Authorization check - 
         print "Stateless proxy request payload - " + str(aConnectedSIPMessage)
 
     def receivedValidConnectedResponseEventHandler(self, aConnectedSIPMessage):
